7256af701461f3c593d175bf9afe978063fa45c9
JhonnySevenn <jhonnyea693@gmail.com>
2022-09-01 13:24:40 -0300

    cambios componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// mis componentes o rutas
+import { TesoreriaComponent } from './components/tesoreria/tesoreria.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/inicio',
+    pathMatch: 'full'
+  },
+  {
+    path: 'inicio',
+    component:InicioComponent,
+  },
+  {
+    path:'tesoreria',
+    component: TesoreriaComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { NavegacionComponent } from './components/navegacion/navegacion.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlider, MatSliderModule } from '@angular/material/slider';
+import { TesoreriaComponent } from './components/tesoreria/tesoreria.component';
+import { MovimientosComponent } from './components/movimientos/movimientos.component';
+
+// angular material
+import {MatTableModule} from '@angular/material/table';
+
+import { TabsComponent } from './components/tabs/tabs.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from "@angular/material/form-field";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    NavegacionComponent,
+    TesoreriaComponent,
+    TabsComponent,
+    MovimientosComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSliderModule,
+    RouterModule,
+    MatTableModule,
+    FormsModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
